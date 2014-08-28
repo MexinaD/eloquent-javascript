cf178bce02744e0643597ae80d253d628f76b4e1
MexinaD <mexinamiel@gmail.com>
2014-08-28 15:20:28 +0300

    Added the coddes of exercise7.1

---
+++ exercise8.1.js
@@ -0,0 +1,13 @@
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+Point.prototype.add = function(other) {
+  return new Point(this.x + other.x, this.y + other.y);
+};
+Point.prototype.isEqualTo = function(other) {
+  return this.x == other.x && this.y == other.y;
+};
+
+console.log((new Point(3, 1)).add(new Point(2, 4)));
+
