804407dbbab0dd2436d79df9aac2e98ec0978deb
MexinaD <mexinamiel@gmail.com>
2014-08-29 13:19:33 +0300

    Added the codes of exercise8.5

---
+++ exercise8.5.js
@@ -0,0 +1,12 @@
+Terrarium.prototype.listSurroundings = function(center) {
+  var result = {};
+  var grid = this.grid;
+  directions.each(function(name, direction) {
+    var place = center.add(direction);
+    if (grid.isInside(place))
+      result[name] = characterFromElement(grid.valueAt(place));
+    else
+      result[name] = "#";
+  });
+  return result;
+};
