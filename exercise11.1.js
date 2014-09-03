8d4a436fca39884e794ecdbc14e39b7375419ccd
MexinaD <mexinamiel@gmail.com>
2014-09-03 15:10:07 +0300

    Added the codes of exercise11.1

---
+++ exercise11.1.js
@@ -0,0 +1,6 @@
+function validInfo(form) {
+  return form.elements.name.value != "" &&
+    /^.+@.+\.\w{2,3}$/.test(form.elements.email.value);
+}
+
+console.log(validInfo(document.forms.userinfo));
