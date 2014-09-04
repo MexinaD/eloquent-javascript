bac7774f50d88430fe4dc28c487591fbfbc8b8ba
MexinaD <mexinamiel@gmail.com>
2014-09-04 14:31:36 +0300

    Added the codes of exercise12.1

---
+++ exercise12.1.js
@@ -0,0 +1,12 @@
+function asHTML(node) {
+  if (isTextNode(node))
+    return escapeHTML(node.nodeValue);
+  else if (node.childNodes.length == 0)
+    return "<" + node.nodeName + "/>";
+  else
+    return "<" + node.nodeName + ">" +
+           map(asHTML, node.childNodes).join("") +
+           "</" + node.nodeName + ">";
+}
+
+console.log(asHTML(document.body));
