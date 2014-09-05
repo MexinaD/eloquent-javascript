3fd570c09d4b6d845f4baec026b75d67ba5cf532
MexinaD <mexinamiel@gmail.com>
2014-09-05 13:31:51 +0300

    Added the codes of exercise13.2

---
+++ exercise13.2.js
@@ -0,0 +1,9 @@
+Square.moveContent = function(target) {
+  target.content = this.content;
+  this.content = null;
+  target.tableCell.appendChild(this.tableCell.lastChild);
+};
+Square.clearContent = function() {
+  this.content = null;
+  removeElement(this.tableCell.lastChild);
+};
